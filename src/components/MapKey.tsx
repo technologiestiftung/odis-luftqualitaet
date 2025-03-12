c6b1fe1696a85899faec88d5fbf5155f95e03eee
hhack <hans.f.hack@googlemail.com>
2025-03-12 11:05:07 +0100

    improved map key highlight 2

--- src/components/MapKey.tsx
+++ src/components/MapKey.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { categoryColors, categoryLabels, fillOpacity } from "@/lib/mapUtils";
+import {
+  categoryColors,
+  categoryLabels,
+  fillOpacity,
+  hexToRgb,
+} from "@/lib/mapUtils";
 
 interface MapKeyProps {
   pollutionValues: object | null; // Expecting values 1-5
 }
-
-const hexToRgb = (hex: string) => {
-  const bigint = parseInt(hex.slice(1), 16);
-  const r = (bigint >> 16) & 255;
-  const g = (bigint >> 8) & 255;
-  const b = bigint & 255;
-  return `${r}, ${g}, ${b}`;
-};
 
 export const MapKey = ({ pollutionValues }: MapKeyProps) => {
   return (
