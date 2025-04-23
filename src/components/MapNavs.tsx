232fed1e794b1aeeb91d94fdf8d9f5b79fd2c694
hhack <hans.f.hack@googlemail.com>
2025-04-23 10:00:50 +0200

    remove ts errors

--- src/components/MapNavs.tsx
+++ src/components/MapNavs.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import maplibregl from "maplibre-gl";
 
 import { ExpandIcon } from "@/components/icons/expand-icon";
-import { CompressIcon } from "@/components/icons/compress-icon";
 import { PlusIcon } from "@/components/icons/plus-icon";
 import { MinusIcon } from "@/components/icons/minus-icon";
 
@@ -11,56 +10,6 @@ interface MapNavsProps {
 }
 
 export const MapNavs = ({ map }: MapNavsProps) => {
-  const [isFullScreen, setIsFullScreen] = useState(false);
-
-  function fullScreen() {
-    const doc = document.documentElement as HTMLElement & {
-      webkitRequestFullscreen?: () => Promise<void>;
-    };
-    const docExit = document as Document & {
-      webkitExitFullscreen?: () => Promise<void>;
-    };
-
-    if (!doc || !document) return;
-
-    // @ts-expect-error placeholder
-    if (!document.fullscreenElement && !docExit.webkitFullscreenElement) {
-      setIsFullScreen(true);
-      if (doc.requestFullscreen) {
-        doc.requestFullscreen();
-      } else if (doc.webkitRequestFullscreen) {
-        doc.webkitRequestFullscreen();
-      }
-    } else {
-      setIsFullScreen(false);
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (docExit.webkitExitFullscreen) {
-        docExit.webkitExitFullscreen();
-      }
-    }
-  }
-
-  useEffect(() => {
-    function handleFullScreenChange() {
-      // @ts-expect-error placeholder
-      if (!document.fullscreenElement && !document.webkitFullscreenElement) {
-        setIsFullScreen(false);
-      }
-    }
-
-    document.addEventListener("fullscreenchange", handleFullScreenChange);
-    document.addEventListener("webkitfullscreenchange", handleFullScreenChange);
-
-    return () => {
-      document.removeEventListener("fullscreenchange", handleFullScreenChange);
-      document.removeEventListener(
-        "webkitfullscreenchange",
-        handleFullScreenChange
-      );
-    };
-  }, []);
-
   return (
     <nav className="z-10 absolute bottom-[8px] right-0 grid p-2">
       <button
